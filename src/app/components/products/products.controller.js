a6822025c0f39e75a07e491b7e390937b3a07a0f
Johannes Schäfer <mail@johannes-schaefer.net>
2016-04-21 09:20:16 +0200

    Retrieve categories and products on customer frontend from API

--- src/app/components/products/products.controller.js
+++ src/app/components/products/products.controller.js
@@ -6,71 +6,39 @@
 		.controller('ProductsController', ProductsController);
 
 	/** @ngInject */
-	function ProductsController(Lightbox) {
+	function ProductsController(Restangular, Lightbox) {
 
 		var vm = this;
+		vm.categories = [];
+		vm.products = [];
 
 		vm.openPreview = function( index ) {
 			Lightbox.openModal( vm.products, index );
 		};
 
-		vm.products = [{
-			title: 'Produkt #1',
-			number: '2014.08.1337',
-			image: {
-				thumb: 'http://dummyimage.com/400x300/ccc/777.jpg',
-				original: 'http://dummyimage.com/800x600/575757/fff.jpg?text=Produkt+0x231'
-			}
-		},{
-			title: 'Produkt #2',
-			number: '2014.08.1338',
-			image: {
-				thumb: 'http://dummyimage.com/400x300/ccc/777.jpg',
-				original: 'http://dummyimage.com/800x600/575757/fff.jpg?text=Produkt+0x232'
-			}
-		},{
-			title: 'Produkt #3',
-			number: '2014.08.1339',
-			image: {
-				thumb: 'http://dummyimage.com/400x300/ccc/777.jpg',
-				original: 'http://dummyimage.com/800x600/575757/fff.jpg?text=Produkt+0x233'
-			}
-		},{
-			title: 'Produkt #4',
-			number: '2014.08.1340',
-			image: {
-				thumb: 'http://dummyimage.com/400x300/ccc/777.jpg',
-				original: 'http://dummyimage.com/800x600/575757/fff.jpg?text=Produkt+0x234'
-			}
-		},{
-			title: 'Produkt #5',
-			number: '2013.08.42',
-			image: {
-				thumb: 'http://dummyimage.com/400x300/ccc/777.jpg',
-				original: 'http://dummyimage.com/800x600/575757/fff.jpg?text=Produkt+0x235'
-			}
-		},{
-			title: 'Produkt #6',
-			number: '2013.08.43',
-			image: {
-				thumb: 'http://dummyimage.com/400x300/ccc/777.jpg',
-				original: 'http://dummyimage.com/800x600/575757/fff.jpg?text=Produkt+0x236'
-			}
-		},{
-			title: 'Produkt #7',
-			number: '2013.08.44',
-			image: {
-				thumb: 'http://dummyimage.com/400x300/ccc/777.jpg',
-				original: 'http://dummyimage.com/800x600/575757/fff.jpg?text=Produkt+0x237'
-			}
-		},{
-			title: 'Produkt #8',
-			number: '2013.08.45',
-			image: {
-				thumb: 'http://dummyimage.com/400x300/ccc/777.jpg',
-				original: 'http://dummyimage.com/800x600/575757/fff.jpg?text=Produkt+0x238'
-			}
-		}];
+		Restangular.all('categories').getList({
+			sort: 'title asc'
+		}).then(function( categories ) {
+
+			Restangular.all('products').getList({
+				sort: 'title asc',
+				active: true
+			}).then(function( products ) {
+
+				var groupedProducts = _.groupBy( products, 'category.id' );
+
+				// Add products to categories
+				_.forEach( categories, function( category, key ) {
+					categories[ key ].products = groupedProducts[ category.id ];
+				});
+
+				vm.categories = categories;
+				vm.products = products;
+
+			});
+
+		});
 
 	}
+
 })();
